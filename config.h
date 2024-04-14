c6f439a87a97bc6faaa522808b09bc4e00703b3e
Trent008 <trenthekman68@gmail.com>
2024-04-13 21:48:28 -0500

    add .sh extension to scripts

--- config.h
+++ config.h
@@ -113,7 +113,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
-static const Key startcommand = {0, 0, spawn, SHCMD ("./dwm/autostart-script")};
+static const Key startcommand = {0, 0, spawn, SHCMD ("/home/dwm-trent/autostart.sh")};
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
