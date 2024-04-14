999b9804dbf19b5a486d1b582c9d44e2fbf560b8
Trent008 <trenthekman68@gmail.com>
2024-04-13 23:04:05 -0500

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 # installs dwm with my .xinitrc status display
-
 cd ~
 sudo echo "$(cat dwm-trent/xinitrc)" > .xinitrc
 cd dwm-trent
--- packages.sh
+++ packages.sh
@@ -1,5 +1,4 @@
 # installs my packages including yay
-
 sudo pacman -Syu
 sudo pacman -S libxinerama libx11 libxft xorg-server xorg-xinit xorg-xsetroot arandr kitty rofi feh picom dunst
 sudo pacman -S lxappearance adapta-gtk-theme ttf-meslo-nerd ttf-nerd-fonts-symbols-mono code nano
