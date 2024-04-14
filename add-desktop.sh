1a004437a66a28a53e84cedb716117e307e26994
Trent008 <trenthekman68@gmail.com>
2024-04-13 22:44:10 -0500

    improve scripts

--- add-desktop.sh
+++ add-desktop.sh
@@ -1,3 +1,3 @@
 # adds dwm.desktop to the display manager
 
-sudo echo "$(cat dwm-trent/dwm.desktop)" > /usr/share/xsessions/dwm.desktop
+sudo echo "$(cat /home/trent/dwm-trent/dwm.desktop)" > /usr/share/xsessions/dwm.desktop
