ffc075d5e414fc7139facb4303a1f4e222ceab20
Trent008 <trenthekman68@gmail.com>
2024-04-13 22:33:38 -0500

    add "add-desktop" script

---
+++ add-desktop.sh
@@ -0,0 +1,3 @@
+# adds dwm.desktop to the display manager
+
+sudo echo "$(cat dwm-trent/dwm.desktop)" > /usr/share/xsessions/dwm.desktop
--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,8 @@
+# add items to autostart here
+
 xsetroot -cursor_name left_ptr &
 dunst &
 picom &
-feh --bg-max /home/trent/dwm/wallpapers &
+feh --bg-max /home/trent/dwm-trent/wallpapers &
 spotify-launcher &
 thorium-browser
