1d2596ba67d656f57a2feba52375f8b1dc66c08f
Trent008 <trenthekman68@gmail.com>
2024-04-15 11:55:14 -0500

    fix package install script typo

--- packages.sh
+++ packages.sh
@@ -2,7 +2,7 @@
 sudo pacman -Syu
 sudo pacman -S libxinerama libx11 libxft xorg-server xorg-xinit xorg-xsetroot arandr kitty rofi feh picom dunst
 sudo pacman -S lxappearance adapta-gtk-theme ttf-meslo-nerd ttf-nerd-fonts-symbols-mono code nano
-sudo pacman -S bluez spotify-launcher nemo payerctl pavucontrol blueberry
+sudo pacman -S bluez spotify-launcher nemo playerctl pavucontrol blueberry
 cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
